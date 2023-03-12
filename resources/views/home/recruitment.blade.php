feadb9bfaf843fd10ae7a77010e9d44fd6b79750
syarif soden <65828433+soden46@users.noreply.github.com>
2023-03-12 16:56:49 +0700

    TA

--- resources/views/home/recruitment.blade.php
+++ resources/views/home/recruitment.blade.php
@@ -11,7 +11,7 @@
                 <div class="panel-heading">
                     <h3 class="panel-title">Persentase Data Caffe Dan Lokasi</h3>
                 </div>
-                <div class="panel-body" align-item="center">
+                <div class="row panel-body" align-item="center">
                     <div id="pie_chart" style="width:450px; height:250px;">
                     </div>
                     <div id="pie_chart2" style="width:450px; height:250px;">
@@ -20,118 +20,117 @@
             </div>
         </div>
     </div>
-</div>
 
-<div class="service">
-    <div class="container">
-        <div class="row">
-            @foreach($caffe as $kafe)
-            <div class="col-lg-4 col-md-6">
-                <div class="service-item">
-                    <div class="service-icon service-img d-flex justify-content-center">
-                        <img src="{{asset('assets/img/bgi.png')}}"></img>
+    <div class="service">
+        <div class="container">
+            <div class="row">
+                @foreach($caffe as $kafe)
+                <div class="col-lg-4 col-md-6">
+                    <div class="service-item">
+                        <div class="service-icon service-img d-flex justify-content-center">
+                            <img src="{{asset('assets/img/bgi.png')}}"></img>
+                        </div>
+                        <h3>{{$kafe->group_caffe}}</h3>
+                        <p>
+                            {{$kafe->jumlah_karyawa}}
+                        </p>
+                        <a class="btn" id="detail" name="detail" href="detailrekrut/{{$kafe->group_caffe}}">Detail Caffe</a>
                     </div>
-                    <h3>{{$kafe->group_caffe}}</h3>
-                    <p>
-                        {{$kafe->jumlah_karyawa}}
-                    </p>
-                    <a class="btn" id="detail" name="detail" href="detailrekrut/{{$kafe->group_caffe}}">Detail Caffe</a>
                 </div>
+                @endforeach
             </div>
-            @endforeach
         </div>
     </div>
-</div>
-<!-- Service End -->
-<script type="text/javascript">
-    $(document).ready(function() {
-        var caffe = <?php echo json_encode($jumlahc); ?>;
-        var options = {
-            chart: {
-                renderTo: 'pie_chart',
-                plotBackgroundColor: null,
-                plotBorderWidth: null,
-                plotShadow: false
-            },
-            title: {
-                text: 'Persentase Caffe berdasarkan Group'
-            },
-            tooltip: {
-                pointFormat: '{series.name}: <b>{point.percentage}%</b>',
-                percentageDecimals: 1
+    <!-- Service End -->
+    <script type="text/javascript">
+        $(document).ready(function() {
+            var caffe = <?php echo json_encode($jumlahc); ?>;
+            var options = {
+                chart: {
+                    renderTo: 'pie_chart',
+                    plotBackgroundColor: null,
+                    plotBorderWidth: null,
+                    plotShadow: false
+                },
+                title: {
+                    text: 'Persentase Caffe berdasarkan Group'
+                },
+                tooltip: {
+                    pointFormat: '{series.name}: <b>{point.percentage}%</b>',
+                    percentageDecimals: 1
 
-            },
-            plotOptions: {
-                pie: {
-                    allowPointSelect: true,
-                    cursor: 'pointer',
-                    dataLabels: {
-                        enabled: true,
-                        color: '#000000',
-                        connectorColor: '#000000',
-                        formatter: function() {
-                            return '<b>' + this.point.name + '</b>: ' + this.percentage.toFixed(0) + ' %';
+                },
+                plotOptions: {
+                    pie: {
+                        allowPointSelect: true,
+                        cursor: 'pointer',
+                        dataLabels: {
+                            enabled: true,
+                            color: '#000000',
+                            connectorColor: '#000000',
+                            formatter: function() {
+                                return '<b>' + this.point.name + '</b>: ' + this.percentage.toFixed(0) + ' %';
+                            }
                         }
                     }
-                }
-            },
-            series: [{
-                type: 'pie',
-                name: 'Jumlah Cabang'
-            }]
-        }
-        myarray = [];
-        $.each(caffe, function(index, val) {
-            myarray[index] = [val.group_caffe, val.count];
+                },
+                series: [{
+                    type: 'pie',
+                    name: 'Jumlah Cabang'
+                }]
+            }
+            myarray = [];
+            $.each(caffe, function(index, val) {
+                myarray[index] = [val.group_caffe, val.count];
+            });
+            options.series[0].data = myarray;
+            chart = new Highcharts.Chart(options);
+
         });
-        options.series[0].data = myarray;
-        chart = new Highcharts.Chart(options);
+        $(document).ready(function() {
+            var provinsi = <?php echo json_encode($provinsi); ?>;
+            var options = {
+                chart: {
+                    renderTo: 'pie_chart2',
+                    plotBackgroundColor: null,
+                    plotBorderWidth: null,
+                    plotShadow: false
+                },
+                title: {
+                    text: 'Persentase Lokasi Caffe'
+                },
+                tooltip: {
+                    pointFormat: '{series.name}: <b>{point.percentage}%</b>',
+                    percentageDecimals: 1
 
-    });
-    $(document).ready(function() {
-        var provinsi = <?php echo json_encode($provinsi); ?>;
-        var options = {
-            chart: {
-                renderTo: 'pie_chart2',
-                plotBackgroundColor: null,
-                plotBorderWidth: null,
-                plotShadow: false
-            },
-            title: {
-                text: 'Persentase Lokasi Caffe'
-            },
-            tooltip: {
-                pointFormat: '{series.name}: <b>{point.percentage}%</b>',
-                percentageDecimals: 1
-
-            },
-            plotOptions: {
-                pie: {
-                    allowPointSelect: true,
-                    cursor: 'pointer',
-                    dataLabels: {
-                        enabled: true,
-                        color: '#000000',
-                        connectorColor: '#000000',
-                        formatter: function() {
-                            return '<b>' + this.point.name + '</b>: ' + this.percentage.toFixed(0) + ' %';
+                },
+                plotOptions: {
+                    pie: {
+                        allowPointSelect: true,
+                        cursor: 'pointer',
+                        dataLabels: {
+                            enabled: true,
+                            color: '#000000',
+                            connectorColor: '#000000',
+                            formatter: function() {
+                                return '<b>' + this.point.name + '</b>: ' + this.percentage.toFixed(0) + ' %';
+                            }
                         }
                     }
-                }
-            },
-            series: [{
-                type: 'pie',
-                name: 'Lokasi Caffe'
-            }]
-        }
-        myarray = [];
-        $.each(provinsi, function(index, val) {
-            myarray[index] = [val.provinsi, val.total1];
+                },
+                series: [{
+                    type: 'pie',
+                    name: 'Lokasi Caffe'
+                }]
+            }
+            myarray = [];
+            $.each(provinsi, function(index, val) {
+                myarray[index] = [val.provinsi, val.total1];
+            });
+            options.series[0].data = myarray;
+            chart = new Highcharts.Chart(options);
+
         });
-        options.series[0].data = myarray;
-        chart = new Highcharts.Chart(options);
+    </script>
 
-    });
-</script>
-
-@endsection
+    @endsection
