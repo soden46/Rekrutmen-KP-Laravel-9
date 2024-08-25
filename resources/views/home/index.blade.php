4b619edc547835e4aa2c82c9e12c8a38d0cce373
Soden46 <syarifsoden22@gmail.com>
2024-08-25 22:46:44 +0700

    first commit

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,286 +1,97 @@
-@extends('layouts.home.master',[
-'title' => 'HOME | Bento Group Indonesia',
-'pageTitle' => 'HOME | Bento Group Indonesia',
+@extends('layouts.home.master', [
+    'title' => 'HOME | Bento Group Indonesia',
+    'pageTitle' => 'HOME | Bento Group Indonesia',
 ])
 @section('content')
-<!-- Carousel Start -->
-<div id="carousel" class="carousel slide" data-ride="carousel">
-    <ol class="carousel-indicators">
-        <li data-target="#carousel" data-slide-to="0" class="active"></li>
-        <li data-target="#carousel" data-slide-to="1"></li>
-        <li data-target="#carousel" data-slide-to="2"></li>
-        <li data-target="#carousel" data-slide-to="3"></li>
-    </ol>
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-            <img src="{{asset('assets/img/merr.jpg')}}" alt="Carousel Image">
-            <div class="carousel-caption">
-                <h1 class="animated fadeInLeft">Bento Group Indonesia</h1>
-            </div>
-        </div>
-
-        <div class="carousel-item ">
-            <img src="{{asset('assets/img/bkgodean.jpg')}}" alt="Carousel Image">
-            <div class="carousel-caption">
-                <h1 class="animated fadeInLeft">Bento Kopi</h1>
-            </div>
-        </div>
-
-        <div class="carousel-item">
-            <img src="{{asset('assets/img/NUansa2.jpg')}}" alt="Carousel Image">
-            <div class="carousel-caption">
-                <h1 class="animated fadeInLeft">NUansa Kopi</h1>
-            </div>
-        </div>
-
-        <div class="carousel-item">
-            <img src="{{asset('assets/img/Menoewa.jpg')}}" alt="Carousel Image">
-            <div class="carousel-caption">
-                <h1 class="animated fadeInLeft">Menoewa Kopi</h1>
+    <!-- Header Start -->
+    <div class="container-fluid hero-header py-2 mb-2">
+        <div class="container py-4">
+            <div class="row g-5 align-items-center">
+                <div class="col-lg-6">
+                    <h1 class="display-4 mb-2 animated slideInDown">Yayasan Produk Halal INDONESIA</h1>
+                    <p class="animated slideInDown">Daftar Sertifikasi halal mandiri lebih cepat dan mudah.</p>
+                    <a href="" class="btn btn-primary py-3 px-4 mt-3 animated slideInDown">Daftar Disini</a>
+                </div>
+                <div class="col-lg-6 animated slideInUp">
+                    <img class="img-fluid animated pulse infinite" style="animation-duration: 3s;"
+                        src="{{ asset('assets/img/hero-1.png') }}" alt="">
+                </div>
             </div>
         </div>
     </div>
 
-    <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
-</div>
-<!-- Carousel End -->
+    <!-- Header End -->
 
-<!-- FAQs Start -->
-<div class="faqs">
-    <div class="container">
-        <div class="section-header">
-            <h2>Daftar Posisi Pekerjaan</h2>
-        </div>
-        <div id="accordion">
-            <div class="card">
-                <div class="card-header">
-                    <a class="card-link collapsed" data-toggle="collapse" href="#collapseOne" aria-expanded="true">
-                        <span>1</span> Barista
-                    </a>
+    <!-- Didukung Oleh Start -->
+    <div class="container-xxl bg-light py-5 my-5" style="margin-top: 0px;">
+        <div class="container py-2">
+            <div class="row g-5 align-items-center">
+                <div class="col-lg-2 col-md-12">
+                    <p class="fs-4 text-secondary mb-0"><b>Didukung Oleh:</b></p>
                 </div>
-                <div id="collapseOne" class="collapse show" data-parent="#accordion">
-                    <div class="card-body">
-                        <p><b>Deskripsi</b></p>
-                        <p>Barista adalah Bagian yang memiliki tanggung jawab untuk membuat dan menyajikan minuman sesuai pesanan dan standar yang sudah ditetapkan.</p>
-                        <p><b>Tugas Barista</b></p>
-                        <p>Berpenampilan baik, Pengetahuan dibagian kitchen, Sehat jasmani dan rohani, Kemampuan umum, Berusia maksimal 25 tahun.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="card">
-                <div class="card-header">
-                    <a class="card-link" data-toggle="collapse" href="#collapseTwo">
-                        <span>2</span> Cook
-                    </a>
-                </div>
-                <div id="collapseTwo" class="collapse" data-parent="#accordion">
-                    <div class="card-body">
-                        <p><b>Deskripsi</b></p>
-                        <p>Cook adalah Bagian yang memiliki tanggung jawab untuk membuat dan menyajikan minuman sesuai pesanan dan standar yang sudah ditetapkan.</p>
-                        <p><b>Tugas Cook</b></p>
-                        <p>Berpenampilan baik, Pengetahuan dibagian kitchen, Sehat jasmani dan rohani, Kemampuan umum, Berusia maksimal 25 tahun.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="card">
-                <div class="card-header">
-                    <a class="card-link" data-toggle="collapse" href="#collapseThree">
-                        <span>3</span> Kebersihan
-                    </a>
-                </div>
-                <div id="collapseThree" class="collapse" data-parent="#accordion">
-                    <div class="card-body">
-                        <p><b>Deskripsi</b></p>
-                        <p>Kebersihan/public area adalah bagian yang memiliki tanggung jawab terhadap kebersihan dan kerapian cafe.</p>
-                        <p><b>Tugas Kebersihan</b></p>
-                        <p>Bertanggung Jawab terhadap kebersihan cafe, Menjaga dan memelihara taman dan tanaman di cafe, Mengontrol kebersihan cafe setiap saat.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="card">
-                <div class="card-header">
-                    <a class="card-link" data-toggle="collapse" href="#collapseFour">
-                        <span>4</span> Kasir
-                    </a>
-                </div>
-                <div id="collapseFour" class="collapse" data-parent="#accordion">
-                    <div class="card-body">
-                        <p><b>Deskripsi</b></p>
-                        <p>Kasir adalah Bagian yang bertugas untuk melayani pelanggan dan menginput transaksi pesanan.</p>
-                        <p><b>Tugas Kasir</b></p>
-                        <p>Melayani tamu atau pelanggan kafe yang datang dengan 5s, Melakukan pencatatan semua transaksi, Memberikan informasi mengenai produk kepada konsumen.</p>
+                <div class="col-lg-10 col-md-12">
+                    <div class="row g-5">
+                        <div class="col-lg-4 col-md-6 text-center wow fadeIn" data-wow-delay="0.1s">
+                            <img class="img-fluid mb-4" src="img/logo-partner1.png" alt="Partner 1">
+                            {{-- <p class="fs-5 text-primary mb-0">Partner 1</p> --}}
+                        </div>
+                        <div class="col-lg-4 col-md-6 text-center wow fadeIn" data-wow-delay="0.3s">
+                            <img class="img-fluid mb-4" src="img/logo-partner2.png" alt="Partner 2">
+                            {{-- <p class="fs-5 text-primary mb-0">Partner 2</p> --}}
+                        </div>
+                        <div class="col-lg-4 col-md-6 text-center wow fadeIn" data-wow-delay="0.5s">
+                            <img class="img-fluid mb-4" src="img/logo-partner3.png" alt="Partner 3">
+                            {{-- <p class="fs-5 text-primary mb-0">Partner 3</p> --}}
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-<!-- FAQs End -->
+    <!-- Didukung Oleh End -->
 
-<!-- Newsletter Start -->
-<div class="about">
-    <div class="container">
-        <div class="row align-items-center">
-            <div class="section-header">
-                <h2>
-                    <h3>Dokumen Yang Harus Dilengkapi</h3>
-                </h2>
-            </div>
-            <div class="about-text">
-                <p><b>Sebelum melakukan pendaftaran silahkan menyiapkan dokumen administrasi sebagai berikut :</b></p>
-                <p>1. Pas Foto ukuran 4 x 6 (berwarna) (Photo colored);</p>
-                <p>2. Scan KTP Asli (ID Card);</p>
-                <p>3. Scan Ijazah / Surat Keterangan Lulus (SKL) (Graduation Certificate);</p>
-                <p>4. Scan Transkrip nilai (Transcript);</p>
-                <p>5. Scan Kartu Keluarga (Family Card);</p>
-                <p>6. CV (Curriculum Vitae);</p>
-                <p>7. Scan Surat Keterangan Berbadan Sehat dari Rumah Sakit/Puskesmas/Klinik (pemeriksaan per September 2022) (Certificate of Health);</p>
-                <p>8. Scan Surat Referensi Pengalaman Bekerja Asli (apabila ada) (Reference Letter);</p>
-                <p>9. Scan Sertifikat Kompetensi/Pelatihan (apabila ada) (Competence Certificate).</p>
-                <p>Keterangan :</p>
-                <p>Harap menggabungkan dokumen sesuai dengan urutan. Dokumen yang diunggah tidak sesuai urutan akan berpengaruh pada proses seleksi administrasi.</p>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- Newsletter End -->
-
-<!-- Testimonial Start -->
-<div class="testimonial">
-    <div class="container">
-        <div class="section-header">
-            <h2>Daftar Caffe</h2>
-        </div>
-        <div class="owl-carousel testimonials-carousel">
-            <div class="testimonial-item">
-                <i class="fa fa-quote-right"></i>
-                <div class="row align-items-center">
-                    <div class="col-3">
-                        <img src="assets/img/bentokopi.jpg" alt="">
-                    </div>
-                    <div class="col-9">
-                        <h2>Bento Kopi</h2>
-                        <!-- <p>Profession</p> -->
-                    </div>
-                    <!-- <div class="col-12">
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam accumsan lacus eget velit
-                            </p>
-                        </div> -->
-                </div>
-            </div>
-            <div class="testimonial-item">
-                <i class="fa fa-quote-right"></i>
-                <div class="row align-items-center">
-                    <div class="col-3">
-                        <img src="assets/img/kopinuri.jpg" alt="">
-                    </div>
-                    <div class="col-9">
-                        <h2>Kopi Nuri</h2>
-                        <!-- <p>Profession</p> -->
-                    </div>
-                    <!-- <div class="col-12">
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam accumsan lacus eget velit
-                            </p>
-                        </div> -->
-                </div>
-            </div>
-            <div class="testimonial-item">
-                <i class="fa fa-quote-right"></i>
-                <div class="row align-items-center">
-                    <div class="col-3">
-                        <img src="assets/img/menoewapng.png" alt="">
-                    </div>
-                    <div class="col-9">
-                        <h2>Monoewa kopi</h2>
-                        <!-- <p>Profession</p> -->
-                    </div>
-                    <!-- <div class="col-12">
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam accumsan lacus eget velit
-                            </p>
-                        </div> -->
-                </div>
-            </div>
-            <div class="testimonial-item">
-                <i class="fa fa-quote-right"></i>
-                <div class="row align-items-center">
-                    <div class="col-3">
-                        <img src="assets/img/nuansajpg.jpg" alt="">
-                    </div>
-                    <div class="col-9">
-                        <h2>NUansa</h2>
-                        <!-- <p>Profession</p> -->
-                    </div>
-                    <!-- <div class="col-12">
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam accumsan lacus eget velit
-                            </p>
-                        </div> -->
-                </div>
-            </div>
-            <div class="testimonial-item">
-                <i class="fa fa-quote-right"></i>
-                <div class="row align-items-center">
-                    <div class="col-3">
-                        <img src="assets/img/kopisini.jpg" alt="">
-                    </div>
-                    <div class="col-9">
-                        <h2>Kopi Sini</h2>
-                        <!-- <p>Profession</p> -->
-                    </div>
-                    <!-- <div class="col-12">
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam accumsan lacus eget velit
-                            </p>
-                        </div> -->
-                </div>
-            </div>
-            <div class="testimonial-item">
-                <i class="fa fa-quote-right"></i>
-                <div class="row align-items-center">
-                    <div class="col-3">
-                        <img src="assets/img/easy.png" alt="">
-                    </div>
-                    <div class="col-9">
-                        <h2>Easy Breezy</h2>
-                    </div>
-
-                </div>
-            </div>
-            <div class="testimonial-item">
-                <i class="fa fa-quote-right"></i>
-                <div class="row align-items-center">
-                    <div class="col-3">
-                        <img src="assets/img/njati.png" alt="">
-                    </div>
-                    <div class="col-9">
-                        <h2>nJati Kopi</h2>
-                    </div>
-                </div>
-            </div>
-            <div class="testimonial-item">
-                <i class="fa fa-quote-right"></i>
-                <div class="row align-items-center">
-                    <div class="col-3">
-                        <img src="assets/img/stako2.png" alt="">
-                    </div>
-                    <div class="col-9">
-                        <h2>stako</h2>
-                    </div>
+    <!-- Tentang Start -->
+    <div class="faqs">
+        <div class="container">
+            <div class="content">
+                <img src="{{ asset('assets/img/hero-1.png') }}" alt="Barista" class="faq-img">
+                <div class="text-content">
+                    <h1>Deskripsi</h1>
+                    <p>Barista adalah Bagian yang memiliki tanggung jawab untuk membuat dan menyajikan minuman
+                        sesuai pesanan dan standar yang sudah ditetapkan.</p>
                 </div>
             </div>
         </div>
     </div>
-</div>
-</div>
-<!-- Testimonial End -->
+    <!-- Tentang End -->
+
+    <!-- Tugas Start -->
+    <div class="faqs">
+        <div class="container">
+            <div class="content">
+                <div class="text-content">
+                    <h1>Deskripsi</h1>
+                    <p>Barista adalah Bagian yang memiliki tanggung jawab untuk membuat dan menyajikan minuman
+                        sesuai pesanan dan standar yang sudah ditetapkan.</p>
+                </div>
+                <img src="{{ asset('assets/img/hero-1.png') }}" alt="Barista" class="faq-img">
+            </div>
+        </div>
+    </div>
+    <!-- Tugas End -->
+
+    <!-- Proses Sertifikasi Start -->
+    <div class="container-xxl py-5">
+        <div class="container">
+            <div class="text-center mx-auto wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
+                <h1 class="display-4">Proses Sertifikasi</h1>
+                <p class="text-primary fs-5 mb-5">Alur Proses Sertifikasi</p>
+            </div>
+            <!-- Gambar dengan animasi fadeInUp -->
+            <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
+                <img src="{{ asset('assets/img/proses-sertifikasi.png') }}" alt="Proses Sertifikasi" class="img-fluid">
+            </div>
+        </div>
+    </div>
+    <!-- Proses Sertifikasi End -->
 @endsection
--- resources/views/layouts/home/navbar.blade.php
+++ resources/views/layouts/home/navbar.blade.php
@@ -2,32 +2,36 @@
     <div class="container-fluid">
 
         <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
-            <a href="#" class="navbar-brand">MENU</a>
+            <a href="#" class="navbar-brand"></a>
             <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-                <span class="navbar-toggler-icon" style="color:gold"></span>
+                <span class="navbar-toggler-icon" style="color:rgb(0, 187, 255)"></span>
             </button>
-
             <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
                 <div class="navbar-nav mr-auto">
-                    <div class="logo">
-                        <a href="/">
-                            <img src="{{asset('assets/img/bgi.png')}}" alt="Logo" width="" height="46">
+                    <div class="logo d-flex align-items-center">
+                        <a href="/" class="d-flex align-items-center">
+                            <img src="{{ asset('assets/img/logo.png') }}" alt="Logo" height="46">
+                            <p class="logo-text mb-0 ml-2 text-white"><b>LPH-YPHI</b></p>
                         </a>
                     </div>
-                    <a href="{{url('recruitment')}}" class="nav-item nav-link">Recruitment</a>
-                    <a href="{{url('daftarberita')}}" class="nav-item nav-link">Berita</a>
-                    <a href="{{url('invesmen')}}" class="nav-item nav-link">Invesment</a>
-                    <a href="{{url('faq')}}" class="nav-item nav-link">FAQ</a>
-                    <a href="{{url('contact')}}" class="nav-item nav-link">Hubungi Kami</a>
-                    
-                </div>
-                <div class="navbar-nav mr-1 d-inline-block">
-                    <a class="nav-item nav-link" href="{{route('login')}}">Masuk</a>
-                </div>
-                <div class="navbar-nav mr-1 d-inline-block">
-                <a class="nav-item nav-link" href="{{route('register')}}">Daftar</a>
+                    <a href="{{ url('recruitment') }}" class="nav-item nav-link">Home</a>
+                    <a href="{{ url('daftarberita') }}" class="nav-item nav-link">Profile</a>
+                    <a href="{{ url('invesmen') }}" class="nav-item nav-link">Struktur Organisasi</a>
+                    <a href="{{ url('faq') }}" class="nav-item nav-link">Auditor Halal</a>
+                    <a href="{{ url('contact') }}" class="nav-item nav-link">Proses Sertifikasi</a>
+                    <a href="{{ url('contact') }}" class="nav-item nav-link">Kontak</a>
+                    <a href="{{ url('contact') }}" class="nav-item nav-link">SIHALAL</a>
                 </div>
             </div>
-        </nav>
+
+
+            {{-- <div class="navbar-nav mr-1 d-inline-block">
+                    <a class="nav-item nav-link" href="{{ route('login') }}">Masuk</a>
+                </div>
+                <div class="navbar-nav mr-1 d-inline-block">
+                    <a class="nav-item nav-link" href="{{ route('register') }}">Daftar</a>
+                </div> --}}
     </div>
+    </nav>
+</div>
 </div>
